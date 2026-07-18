36a78c2c4165fca49fdfd7f364235103cf789c9b
Patel Jivan <patel.jivan.cg@gmail.com>
2026-07-18 17:34:11 +0530

    Update main app layout configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Drawer } from 'expo-router/drawer';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
@@ -13,12 +14,20 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Drawer>
+          <Drawer.Screen name="(tabs)" options={{ drawerLabel: 'Dashboard', title: 'Dashboard', headerShown: false }} />
+          <Drawer.Screen name="camera" options={{ drawerLabel: 'Camera', title: 'Camera' }} />
+          <Drawer.Screen name="contacts" options={{ drawerLabel: 'Contacts', title: 'Contacts' }} />
+          <Drawer.Screen name="location" options={{ drawerLabel: 'Location', title: 'Location' }} />
+          <Drawer.Screen name="clipboard" options={{ drawerLabel: 'Clipboard', title: 'Clipboard' }} />
+          <Drawer.Screen name="settings" options={{ drawerLabel: 'Settings', title: 'Settings' }} />
+          <Drawer.Screen name="previewSurway" options={{ drawerLabel: 'Preview Survey', title: 'Preview Survey' }} />
+          <Drawer.Screen name="modal" options={{ drawerItemStyle: { display: 'none' } }} />
+        </Drawer>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
